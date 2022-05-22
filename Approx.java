1ba9281ade16151424ef531ba2970fcc6fe68830
Gregory Stocker <gregory.stocker2@gmail.com>
2022-05-22 16:46:45 -0700

    Update Approx.java

--- Approx.java
+++ Approx.java
@@ -16,7 +16,7 @@ public class Approx {
     static double elapsedTime = endTime - startTime;
     static double acceleration = 9.8;
 
-    //calculates the area of a function 
+    //calculates the area under a function 
     static double reinmann(int numRectangles, double start, double end) {
         double deltaX = deltaX(start, end, numRectangles);
         double sum = 0;
